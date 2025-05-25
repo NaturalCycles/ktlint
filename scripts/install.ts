4ac5fa25da3c2cd3cf15efe5041387e605e553b0
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-05-25 19:03:06 +0200

    feat: ktlint 1.6.0

--- scripts/install.ts
+++ scripts/install.ts
@@ -1,6 +1,6 @@
 /*
 
-yarn tsx install
+pn tsx scripts/install.ts
 
  */
 
@@ -22,7 +22,7 @@ void (async () => {
   // https://github.com/pinterest/ktlint/issues/1195
   // Hence, reverting to 0.40.0
   // const ktlintVersion = '0.43.2'
-  const ktlintVersion = '1.5.0'
+  const ktlintVersion = '1.6.0'
   console.log(`downloading ktlint ${ktlintVersion} from github, may take some time...`)
 
   const url = `https://github.com/pinterest/ktlint/releases/download/${ktlintVersion}/ktlint`
