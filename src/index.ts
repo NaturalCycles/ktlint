3c6438fb56c46b2bc8838e37dd5569679d9535c6
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2025-08-25 17:11:16 +0200

    fix: allow to run `ktlintAll([])` without `-F`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import type { SpawnOptions } from 'node:child_process'
 import { runCommandSafe } from './exec.util.js'
 import { ktlintPath } from './paths.js'
 
-export async function ktlintAll(args: string[] = [], opt?: SpawnOptions): Promise<void> {
-  await runCommandSafe(ktlintPath, ['-F', ...args], opt)
+export async function ktlintAll(args: string[] = ['-F'], opt?: SpawnOptions): Promise<void> {
+  await runCommandSafe(ktlintPath, args, opt)
 }
